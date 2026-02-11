6c66518973c5eb7957e7e01974c1922358bc32ec
Thiago Augusto <wstagf@gmail.com>
2026-02-11 11:33:37 -0300

    Implementado Hello Word

---
+++ Aula01/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // escreve no terminal, comando de saida
+    printf("Ola PUC 2026 \n");
+
+    int numero = 100;
+
+    printf("%d\n", numero);
+    
+    return 0;
+}
