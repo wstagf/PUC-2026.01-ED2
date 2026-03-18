081f7fe52dd114d125b5c95ca8649f1ed6299a89
Thiago Augusto <wstagf@gmail.com>
2026-03-18 10:08:17 -0300

    funcao que cria o NO

--- Aula05/arvore.c
+++ Aula05/arvore.c
@@ -7,3 +7,15 @@ typedef struct No {
     struct No* dir;
 }
 
+No* criarNo(int vlr) {
+    No* novo = 
+        (No*) malloc(sizeof(No));
+    if (novo == NULL) {
+        printf("Erro de alocacao \n");
+        exit(1);
+    }
+    novo->valor = vlr;
+    novo->esq = NULL;
+    novo->dir = NULL;
+}
+
